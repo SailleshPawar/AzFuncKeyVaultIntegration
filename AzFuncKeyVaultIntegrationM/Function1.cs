3a013c347d52bff4f4fd5159120c292f1a4c4713
Saillesh Pawar <Sailleshp@quinnox.com>
2019-04-18 20:06:05 +0530

    Add project files.

---
+++ AzFuncKeyVaultIntegrationM/Function1.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Services;
+
+namespace AzFuncKeyVaultIntegrationM
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequestMessage req, TraceWriter log)
+        {
+            log.Info("C# HTTP trigger function processed a request.");
+            KeyVaultService _service = new KeyVaultService();
+
+            //secret name applicationSecret2
+            string secretValue = await _service.GetSecretValue("applicationSecret2");
+            log.Info("Secret value retrived via Secret Uri" + secretValue);
+            // parse query parameter
+            string name = req.GetQueryNameValuePairs()
+                .FirstOrDefault(q => string.Compare(q.Key, "name", true) == 0)
+                .Value;
+
+            if (name == null)
+            {
+                // Get request body
+                dynamic data = await req.Content.ReadAsAsync<object>();
+                name = data?.name;
+            }
+
+            return name == null
+                ? req.CreateResponse(HttpStatusCode.BadRequest, "Please pass a name on the query string or in the request body")
+                : req.CreateResponse(HttpStatusCode.OK, $"Hello {name} using keyvault Syntax from app settings {Environment.GetEnvironmentVariable("secret2")}");
+        }
+    }
+}
---
+++ Services/KeyVaultService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class KeyVaultService
+    {
+        public  async Task<string> GetSecretValue(string keyName)
+        {
+            string secret = "";
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            //slow without ConfigureAwait(false)
+            //keyvault should be keyvault DNS Name
+            var secretBundle = await keyVaultClient.GetSecretAsync(Environment.GetEnvironmentVariable("keyvault") + keyName).ConfigureAwait(false);
+            secret = secretBundle.Value;
+            Console.WriteLine(secret);
+            return secret;
+        }
+
+    }
+}
